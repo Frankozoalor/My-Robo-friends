3890723e85694d0cd46677b34cb410eb8c079c7b
EkeneOzoalor <88558745+EkeneOzoalor@users.noreply.github.com>
2022-01-10 04:46:25 -0800

    Updates

---
+++ precache-manifest.1302a0f020e8e28c1900b58fd4b6759b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4788fb02af9a76052adfbc5052acdab",
+    "url": "/My-Robo-friends/index.html"
+  },
+  {
+    "revision": "1b1a4133686499da29aa",
+    "url": "/My-Robo-friends/static/css/2.3b2cfc2e.chunk.css"
+  },
+  {
+    "revision": "f8f996687095114568b6",
+    "url": "/My-Robo-friends/static/css/main.c558f05a.chunk.css"
+  },
+  {
+    "revision": "1b1a4133686499da29aa",
+    "url": "/My-Robo-friends/static/js/2.d679fc0b.chunk.js"
+  },
+  {
+    "revision": "312ff34b36bcb6695a4b",
+    "url": "/My-Robo-friends/static/js/3.d3c67870.chunk.js"
+  },
+  {
+    "revision": "f8f996687095114568b6",
+    "url": "/My-Robo-friends/static/js/main.e82134d1.chunk.js"
+  },
+  {
+    "revision": "8ed23e4b4b4641d7a264",
+    "url": "/My-Robo-friends/static/js/runtime~main.656fcb1e.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/My-Robo-friends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
